fc966778be2cd3d81c2c1aa0cc9e7ea90cf0ac54
laxman-sagar <140710306+laxman-sagar@users.noreply.github.com>
2023-08-13 20:21:16 +0530

    Update cors

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-app.use(cors({ origin: 'https://bookinnng.onrender.com/', credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRoute);
